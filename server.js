1e7d321ad7a605032ca8a3016fbecaf333f35fc7
John Tucker <jtucker@spotify.com>
2021-03-07 08:19:52 -0700

    increment

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const grpc = require('@grpc/grpc-js');
 const messages = require('./helloworld/helloworld_pb');
 const services = require('./helloworld/helloworld_grpc_pb');
 
-console.log("test");
+const sayHello = (call, callback) => {
+  const reply = new messages.HelloReply();
+  reply.setMessage('Hello ' + call.request.getName());
+  callback(null, reply);
+}
+  
+server = new grpc.Server();
+server.addService(services.GreeterService, { sayHello });
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+  server.start();
+});
